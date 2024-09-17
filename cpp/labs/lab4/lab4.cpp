37d43c26018d358f6b0a9cd47509ec2515948672
Svitlichnyi Dmitro <Svetlicnyjdima0@gmail.com>
2024-09-17 20:30:30 +0300

    Added comments to lab4

--- cpp/labs/lab4/lab4.cpp
+++ cpp/labs/lab4/lab4.cpp
@@ -4,14 +4,13 @@
  * Група студента: 123
  * Lab 1.4
  */
-//9 варіант
 #include <iostream>
 #include <math.h>
 int main(){
-    unsigned short sA;
-    unsigned short *psB;
-    psB = &sA;
-    *psB = 612;
+    unsigned short sA; //Cтворення змінної
+    unsigned short *psB; //Створення вказівника
+    psB = &sA; //Ініціювання вказівних змінних адресами змінних
+    *psB = 612; //Присвоєння значення змінній
     int iA;
     int *piB;
     piB = &iA;
@@ -24,14 +23,14 @@ int main(){
     double *pdB;
     pdB = &dA;
     *pdB = -30.22e100;
-    void *pV;
-    int sizesA = sizeof(sA);
-    int sizepsA = sizeof(psB);
+    void *pV; //Створення нетипізованої вказівної зміни
+    int sizesA = sizeof(sA); //Створення змінної яка дорівнює розміру змінної
+    int sizepsA = sizeof(psB); //Створення змінної яка дорівнює розміру вказівної змінної
     int sizeiA = sizeof(iA);
     int sizepiB = sizeof(piB);
     int sizefA = sizeof(fA);
     int sizepfB = sizeof(pfB);
     int sizedA = sizeof(dA);
     int sizepdB = sizeof(pdB);
-    pV = &psB;
+    pV = &psB; //Ініціювання нетипізованої вказівної змінної адресом типізованої вказівної змінної
 }
