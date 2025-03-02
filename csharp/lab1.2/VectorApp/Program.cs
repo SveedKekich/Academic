5e8cc2ac56f1e767c09e0862c1adf6971ca3d871
Svitlichnyi Dmitro <Svetlicnyjdima0@gmail.com>
2025-03-02 20:01:52 +0200

    Added lab1.2 to csharp

---
+++ csharp/lab1.2/VectorApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using VectorLibrary;
+
+class Program
+{
+    static void Main()
+    {
+        // Використання різних конструкторів
+        Vector v1 = new Vector(); // Конструктор за замовчуванням
+        Vector v2 = new Vector(10, 45); // Параметризований конструктор
+        Vector v3 = new Vector(v2); // Конструктор копіювання
+
+        Console.WriteLine($"Vector v2 (Length: {v2.Length}, Angle: {v2.Angle}°)");
+
+        v2.GetEndCoordinates(out double x, out double y);
+        Console.WriteLine($"End coordinates of v2: ({x}, {y})");
+    }
+}
---
+++ csharp/lab1.2/VectorLibrary/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace VectorLibrary
+{
+    public class Vector
+    {
+        private double length;
+        private double angle; // Кут у градусах
+
+        // Конструктори
+        public Vector()
+        {
+            length = 0;
+            angle = 0;
+        }
+
+        public Vector(double len, double ang)
+        {
+            length = len;
+            angle = ang;
+        }
+
+        public Vector(Vector other)
+        {
+            length = other.length;
+            angle = other.angle;
+        }
+
+        // Властивості 
+        public double Length => length;
+        public double Angle => angle;
+
+        // Метод обчислення координат кінця вектора
+        public void GetEndCoordinates(out double x, out double y)
+        {
+            double rad = angle * Math.PI / 180.0;
+            x = length * Math.Cos(rad);
+            y = length * Math.Sin(rad);
+        }
+    }
+}
